1f4aacafdd73cddf5766b7ea7df902239c26d014
oneilld136 <oneilld136@gmail.com>
2018-08-02 04:04:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,2 @@
 katz_deli = []
 
-def line(x)
-  line_array = []
-end
-  if x.length == 0
-    puts "The line is currently empty."
-  else
-    x.each.with_index(1) do |name, index|
-      line_array.push("#{index}. #{name}")
-end
