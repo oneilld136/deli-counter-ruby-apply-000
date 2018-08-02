41a90e2b1c5e91f677a73fea572329d8c9f55ce5
oneilld136 <oneilld136@gmail.com>
2018-08-02 04:27:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 katz_deli = []
+
 def line(x)
-line_array = []
-end
-if x.length == 0
-  puts "The line is currently empty"
-else
-  x.each.with_index(1) do |name, index|
-    line_array.push("#{index}. #{name}")
-end
-puts "The line is currently: #{line_array.join(', ')}"  // 'Name,Index'
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty"
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+  end
+  puts "The line is currently: #{line_array.join(', ')}"  // 'Name,Index'
 end
 def take_a_number (katz_deli, name)
   katz_deli['name'].push(name)
