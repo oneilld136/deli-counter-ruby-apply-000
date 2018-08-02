def3058b86c79db730272045744a3ef1395050ad
oneilld136 <oneilld136@gmail.com>
2018-08-02 04:14:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,17 @@ end
 end
 puts "The line is currently: #{line_array.join(', ')}"  // 'Name,Index'
 end
+end
+def take_a_number (katz_deli, name)
+  katz_deli['name'].push(name)
+  p = katz_deli.index(name)+1
+  puts "Welcome,#{name}. You are number #{katz_deli.length}."
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[1]}."
+    array.shift
+  end
+end
